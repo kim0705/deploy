5f39d21881d735604e93f833ac875823bd665688
kim0705 <dbwls138@naver.com>
2024-09-27 12:45:16 +0900

    init

---
+++ src/main/java/com/study/deploy_back/controller/TestController.java
@@ -0,0 +1,20 @@
+package com.study.deploy_back.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @Value("${client.address}")
+    private String clientAddress;
+
+    @CrossOrigin(originPatterns = "*")
+    @GetMapping("/test")
+    public ResponseEntity<?> test() {
+        return ResponseEntity.ok(clientAddress + "test");
+    }
+}
